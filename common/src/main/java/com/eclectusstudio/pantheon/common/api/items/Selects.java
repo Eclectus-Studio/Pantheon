7e91d3c5b68fb149108c720e135eb9554c5b0af7
Minetrio1256 <141367569+Minetrio1256@users.noreply.github.com>
2026-07-28 09:42:45 -0400

    minor fix + version bump

--- common/src/main/java/com/eclectusstudio/pantheon/common/api/items/Selects.java
+++ common/src/main/java/com/eclectusstudio/pantheon/common/api/items/Selects.java
@@ -2,15 +2,17 @@ package com.eclectusstudio.pantheon.common.api.items;
 
 import com.eclectusstudio.pantheon.common.resource.items.node.ItemModelNode;
 import com.eclectusstudio.pantheon.common.resource.items.node.SelectNode;
+import com.eclectusstudio.pantheon.common.resource.items.node.SelectProperty;
 
 import java.util.Arrays;
+import java.util.List;
 
 public final class Selects {
 
     private Selects() {}
 
     public static SelectNode.Case when(
-            Object value,
+            String value,
             ItemModelNode model
     ) {
         return new SelectNode.Case(
@@ -19,8 +21,28 @@ public final class Selects {
         );
     }
 
+    public static SelectNode.Case when(
+            List<String> values,
+            ItemModelNode model
+    ) {
+        return new SelectNode.Case(
+                values,
+                model
+        );
+    }
+
+    public static SelectNode.Case when(
+            ItemModelNode model,
+            String... values
+    ) {
+        return new SelectNode.Case(
+                Arrays.asList(values),
+                model
+        );
+    }
+
     public static SelectNode select(
-            String property,
+            SelectProperty property,
             ItemModelNode fallback,
             SelectNode.Case... cases
     ) {
