cf6f878f25bf9162a08e7b06406339c94ba7dcc8
Minetrio1256 <141367569+Minetrio1256@users.noreply.github.com>
2026-07-30 11:26:07 -0400

    sword weapon

--- 26.2/paper/src/main/java/com/eclectusstudio/pantheon/item/TexturedWeaponItem.java
+++ 26.2/paper/src/main/java/com/eclectusstudio/pantheon/item/TexturedWeaponItem.java
@@ -3,16 +3,52 @@ package com.eclectusstudio.pantheon.item;
 import com.eclectusstudio.pantheon.common.ResourceLocation;
 import static com.eclectusstudio.pantheon.utils.LocationToNamespaceKey.toKey;
 
+import io.papermc.paper.datacomponent.DataComponentTypes;
+import io.papermc.paper.registry.RegistryAccess;
+import io.papermc.paper.registry.RegistryKey;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.attribute.AttributeModifier;
+import org.bukkit.damage.DamageType;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
-
 public abstract class TexturedWeaponItem extends DurableTexturedItem {
+    private DamageType damageType;
 
     private final double attackDamage;
     private final double attackSpeed;
+
+    protected TexturedWeaponItem(
+            ResourceLocation id,
+            ItemStack itemStack,
+            int maxDurability,
+            ItemStack repairMaterial,
+            DamageType damageType,
+            double attackDamage,
+            double attackSpeed
+    ) {
+        super(id, itemStack, maxDurability, repairMaterial);
+        this.damageType = damageType;
+
+        this.attackDamage = attackDamage;
+        this.attackSpeed = attackSpeed;
+    }
+
+    protected TexturedWeaponItem(
+            ResourceLocation id,
+            ItemStack itemStack,
+            int maxDurability,
+            ItemStack repairMaterial,
+            ResourceLocation damageType,
+            double attackDamage,
+            double attackSpeed
+    ) {
+        super(id, itemStack, maxDurability, repairMaterial);
+        this.damageType = RegistryAccess.registryAccess().getRegistry(RegistryKey.DAMAGE_TYPE).get(toKey(damageType));
+
+        this.attackDamage = attackDamage;
+        this.attackSpeed = attackSpeed;
+    }
 
     protected TexturedWeaponItem(
             ResourceLocation id,
@@ -26,6 +62,10 @@ public abstract class TexturedWeaponItem extends DurableTexturedItem {
 
         this.attackDamage = attackDamage;
         this.attackSpeed = attackSpeed;
+    }
+
+    public DamageType getDamageType() {
+        return damageType;
     }
 
     public double getAttackDamage() {
@@ -58,6 +98,10 @@ public abstract class TexturedWeaponItem extends DurableTexturedItem {
         meta.addAttributeModifier(Attribute.ATTACK_SPEED, attackSpeedModifier);
 
         itemStack.setItemMeta(meta);
+
+        if(damageType != null){
+            itemStack.setData(DataComponentTypes.DAMAGE_TYPE, damageType);
+        }
 
         return itemStack;
     }
--- 26.2/paper/src/main/java/com/eclectusstudio/pantheon/item/weapons/TexturedSwordItem.java
+++ 26.2/paper/src/main/java/com/eclectusstudio/pantheon/item/weapons/TexturedSwordItem.java
@@ -1,4 +1,11 @@
 package com.eclectusstudio.pantheon.item.weapons;
 
-public class TexturedSwordItem {
+import com.eclectusstudio.pantheon.common.ResourceLocation;
+import com.eclectusstudio.pantheon.item.TexturedWeaponItem;
+import org.bukkit.inventory.ItemStack;
+
+public class TexturedSwordItem extends TexturedWeaponItem {
+    protected TexturedSwordItem(ResourceLocation id, ItemStack itemStack, int maxDurability, ItemStack repairMaterial, double attackDamage, double attackSpeed) {
+        super(id, itemStack, maxDurability, repairMaterial, attackDamage, attackSpeed);
+    }
 }
