833b08b700ff953fa43c9d108574e72a0fb2240a
Minetrio1256 <141367569+Minetrio1256@users.noreply.github.com>
2026-08-04 12:27:08 -0400

    version bump and copy asset crash fix

--- common/src/main/java/com/eclectusstudio/pantheon/common/ResourcePackContext.java
+++ common/src/main/java/com/eclectusstudio/pantheon/common/ResourcePackContext.java
@@ -2,7 +2,6 @@ package com.eclectusstudio.pantheon.common;
 
 import java.io.*;
 import java.net.URISyntaxException;
-import java.net.URL;
 import java.nio.file.*;
 import java.security.CodeSource;
 import java.util.Enumeration;
@@ -10,23 +9,24 @@ import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 
 public class ResourcePackContext {
-    public static void extractFolder(String resourceFolder, Path destination)
-            throws IOException, URISyntaxException, ClassNotFoundException {
-
-        Class<?> caller = getCallerClass();
+    public static void extractFolder(
+            Class<?> sourceClass,
+            String resourceFolder,
+            Path destination
+    ) throws IOException, URISyntaxException {
 
         if (!resourceFolder.endsWith("/")) {
             resourceFolder += "/";
         }
 
-        CodeSource codeSource =
-                caller.getProtectionDomain().getCodeSource();
+        CodeSource codeSource = sourceClass
+                .getProtectionDomain()
+                .getCodeSource();
 
         Path jarPath = Path.of(codeSource.getLocation().toURI());
 
         try (JarFile jar = new JarFile(jarPath.toFile())) {
-
-            var entries = jar.entries();
+            Enumeration<JarEntry> entries = jar.entries();
 
             while (entries.hasMoreElements()) {
                 JarEntry entry = entries.nextElement();
@@ -47,31 +47,14 @@ public class ResourcePackContext {
 
                 if (entry.isDirectory()) {
                     Files.createDirectories(target);
-                    continue;
-                }
+                } else {
+                    Files.createDirectories(target.getParent());
 
-                Files.createDirectories(target.getParent());
-
-                try (InputStream in = jar.getInputStream(entry)) {
-                    Files.copy(in, target,
-                            StandardCopyOption.REPLACE_EXISTING);
+                    try (InputStream in = jar.getInputStream(entry)) {
+                        Files.copy(in, target, StandardCopyOption.REPLACE_EXISTING);
+                    }
                 }
             }
         }
-    }
-
-    private static Class<?> getCallerClass()
-            throws ClassNotFoundException {
-
-        StackTraceElement[] stack = Thread.currentThread().getStackTrace();
-
-        // 0 = Thread.getStackTrace
-        // 1 = getCallerClass
-        // 2 = extractFolder
-        // 3 = actual caller
-
-        String callerClassName = stack[3].getClassName();
-
-        return Class.forName(callerClassName);
     }
 }
